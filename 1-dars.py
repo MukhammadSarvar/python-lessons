1707ea49129a3bb9c417b402e1e70a53bb15c55e
MukhammadSarvar <elomonovsarvar09@gmail.com>
2024-07-01 11:18:38 +0500

    Add files via upload
    pytho lessons

---
+++ 1-dars.py
@@ -0,0 +1,38 @@
+# print() funksiyasi
+# 4/24/2024
+
+
+print ("hello world")  # matn chop qilish uchun qo'shtirnoqdan foydalaniladi
+
+print('Hayirli tong!') # matn chop qilish uchun bir tirnoqdan foydalansa ham
+# bo'ladi 
+
+print ("Men  \"RUCHKA\" sotib oldim ") #agar qo'shtirnoq ichida qo'shtirnoqli 
+#so'z bo'lsa back slesh dan foydalanamiz
+
+print(""" kecha keldim,
+ olami kelmadi """) # bunda biz uchta qo'shtirnoq orqali qatorli so'zlar 
+#hiqarishimiz mumkin
+
+print(" o'l sarvi \ngulru kelmadi") # also biz faqat bitta qo'shtirnoq orqali 
+#ham qatorli gaplarda foydalansak bo'ladi faqat unda biz"\n" qo'yib ketishimiz kerak
+
+print('kecha o\'l \nsarvi go\'lru kelmadi ') # agar bir tirnoqdan foydalansak
+# unda back slesh ishlatamiz
+
+
+#33////////
+
+print(2+6*5)# oddiy arifmetikada foydalanamiz
+
+print( 80/5)# bo'lish uchun ishlatamiz
+
+print(89/7) #asosan qoldiqli bo'lishda 
+
+print(60//4)# bu esa qoldiqsiz bo'lishda 
+
+print(3**4)# sonlarni darajasi
+
+print ("Ikkining o'ninchi darajasi ", 2**10, "ga teng") #matn bilan kelganda
+
+print ("3*3=", 3*3)# son orqali ifodalansa
